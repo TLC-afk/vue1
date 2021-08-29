d051720e0f5cf16f6099336ae6986d275ccb423c
Mr.tang <1261651023@qq.com>
2021-08-29 17:10:54 +0800

    模块化：把Money.vue分成四个组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li> <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Tags'
+};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      background: #d9d9d9;
+      margin-right: 12px;
+      height: 24px;
+      border-radius: 12px;
+      padding: 0 16px;
+      line-height: 24px;
+      margin-top: 6px;
+    }
+  }
+
+  > .new {
+    margin-top: 10px;
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+      color: #999;
+    }
+  }
+}
+
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">收入</li>
+      <li>支出</li>
+    </ul>
+  </div>
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Types'
+};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  display: flex;
+  height: 64px;
+  background: #c4c4c4;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      background: black;
+      height: 3px;
+      width: 100%;
+    }
+  }
+}
+
+</style>
