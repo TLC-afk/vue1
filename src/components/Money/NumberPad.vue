9078ac00156934ba6dcffecba66d7c47a80e2c4e
Mr.tang <1261651023@qq.com>
2021-08-30 17:22:46 +0800

    NumberPad 基本完成

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,34 +1,70 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="outputContent">1</button>
+      <button @click="outputContent">2</button>
+      <button @click="outputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="outputContent">4</button>
+      <button @click="outputContent">5</button>
+      <button @click="outputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="outputContent">7</button>
+      <button @click="outputContent">8</button>
+      <button @click="outputContent">9</button>
+      <button class="ok" @click="ok">OK</button>
+      <button class="zero" @click="outputContent">0</button>
+      <button @click="outputContent">.</button>
     </div>
   </div>
 
 </template>
 
 <script lang="ts">
-export default {
-  name: 'NumberPad'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output: string = '0';
+
+  outputContent(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    const input = button.textContent as string;
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0) {
+      if (input === '.') {return;}
+    }
+    this.output += input;
+  }
+
+  remove() {
+    if(this.output.length !== 1){
+      this.output = this.output.slice(0, -1);
+    }else{
+      this.output = '0'
+    }
+  }
+
+  clear() {
+    this.output = '0'
+  }
+
+  ok() {}
+}
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   .output {
     font-family: Consolas, monospace;
@@ -36,6 +72,7 @@ export default {
     padding: 9px 0;
     text-align: right;
     box-shadow: inset 0 -5px 5px -5px fade-out(black, 0.6), inset 0 5px 5px -5px fade-out(black, 0.6);
+    min-height: 72px;
   }
 
   .buttons {
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -12,7 +12,6 @@
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator';
 
-
 @Component
 export default class Types extends Vue {
   type = '+';
