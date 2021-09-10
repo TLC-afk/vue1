00712452c7416f36494ea2ccee3483d115dc4025
Mr.tang <1261651023@qq.com>
2021-09-10 20:53:45 +0800

    删除Type.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,7 +16,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
@@ -26,7 +25,7 @@ import typeList from '@/constant/typeList';
 
 
 @Component({
-  components: {Tabs, Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem,  NumberPad},
 })
 
 export default class Money extends Vue {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -14,13 +14,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
-import Types from '@/components/Money/Types.vue';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constant/intervalList';
 import typeList from '@/constant/typeList';
 
 @Component({
-  components: {Tabs, Types}
+  components: {Tabs}
 })
 export default class Statistics extends Vue {
   type = '';
